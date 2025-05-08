dc122c94cbd2556d9809ea7815fece258cb580d4
diegogrebate <diego.grebate@gmail.com>
2025-05-08 16:31:34 +0200

    gateway to dashboard from landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Button asChild>
+        <Link href={"/dashboard"}>Go to Dashboard</Link>
+      </Button>
+    </div>
+  );
 }
